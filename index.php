b4c08934f5b24870c3624c5c9893ba1d2c409083
MariaPro10 <90986572+MariaPro10@users.noreply.github.com>
2021-10-02 20:35:59 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="he">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ליווי התפתחותי</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="JS/load-common-html.js"></script>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</head>
+
+<body>
+    <div id="common-html"></div>
+    <!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+  <h3 class="margin">מדריכת הליווי ההתפתחותי</h3>
+  <img src="/media/images/mom-and-baby.png" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350">
+    <p>!נעים להכיר</p>
+    <p>מאז שאני זוכרת את עצמי, הייתי מרותקת לעולמם של התינוקות והילדים והיה ברור שאקח את העניין לתחום</p>
+    <p>,המקצועי. אני מומחית להתפתחות בגיל הרך, בעלת תואר שני בתחום, משלבת בין שיטות האבחון והטיפול השונות</p>
+    <p>.ומלמדת את תחום ההתפתחות בקורסים ובהשתלמויות והכשרות לאנשי מקצוע ובעיקר נהנית מהקשר עם ההורים והתינוקות למעלה מ20 שנה</p>
+    <p>.מזמינה אתכם לבקר באתר ובקליניקה ונמצאת כאן בשבילכם לכל שאלה או התלבטות</p>
+    
+</div>
+
+<!-- Second Container -->
+<div class="container-fluid bg-2 text-center">
+    <h3 class="margin">!ברוכים הבאים לקליניקה ולקהילה שלנו</h3>
+    <p>.אתם מוזמנים לסייר באתר ולהנות משלל התכנים המוצעים להורים בתחום התפתחות ילדיכם</p>
+    <p>.האתר כולל מגוון תכנים בתחומי התפתחות הילד הנוגע לשלבי התפתחות וטווחי גיל שונים עד גיל שנתיים</p>
+    <p>.הרשמו לאתר ותהנו מכלל התכנים של הקליניקה</p>
+    <a href="/includes/php/sign-up.php" class="btn btn-default btn-lg">
+        <span class="glyphicon glyphicon-plus"></span> הרשמו פה
+        </a>
+</div>
+
+<!-- Third Container (Grid) -->
+<div class="container-fluid bg-3 text-center">    
+  <h3 class="margin">?מה ניתן למצוא באתר שלנו</h3><br>
+  <div class="row">
+  
+    <!--<div class="col-sm-4">-->
+    <!--    <p>תכנים בתחום הליווי ההתפתחותי של תינוקות וילדים, מידע על שלבי התפתחות שונים</p>-->
+    <!--    <p>.כגון זחילה, התהפכות ועוד, ומידע לפי טווחי גילאים עד גיל שנתיים</p>-->
+    <!--    <img src="/media/images/baby2.jpg" class="img-responsive margin" style="width:100%" alt="Image">-->
+    <!--</div>-->
+  
+    <div class="col-sm-4">
+        <h3 class="margin">מגוון תכנים לפי שלבי התפתחות וגיל</h3>
+        <p>תכנים בתחום הליווי ההתפתחותי של תינוקות וילדים, מידע על שלבי התפתחות שונים</p>
+        <p>.כגון זחילה, התהפכות ועוד, ומידע לפי טווחי גילאים עד גיל שנתיים</p>
+        <!--<a href="/includes/php/Articles.php"><img src="/media/images/baby2.jpg" class="img-responsive margin" style="width:100%" alt="Image"></a>-->
+    </div>
+   
+    <!--<div class="col-sm-4"> -->
+    <!--    <p>קביעת פגישה מקוונת ללא תלות ביצירת קשר טלפוני עם הקליניקה</p>-->
+    <!--    <p>.בזמנכם הפנוי בכל שעה (לרשומים לאתר בלבד)</p>-->
+    <!--    <img src="/media/images/baby3.jpg" class="img-responsive margin" style="width:100%" alt="Image">-->
+    <!--</div>-->
+   
+    <div class="col-sm-4"> 
+        <h3 class="margin">קביעת פגישה אישית מקוונת</h3>
+        <p>קביעת פגישה מקוונת ללא תלות ביצירת קשר טלפוני עם הקליניקה</p>
+        <p>.בזמנכם הפנוי בכל שעה (לרשומים לאתר בלבד)</p>
+        <!--<a href="/includes/php/sign-up.php"><img src="/media/images/baby3.jpg" class="img-responsive margin" style="width:100%" alt="Image"></a>-->
+    </div>
+  
+    <!--<div class="col-sm-4"> -->
+    <!--    <p>קטלוג המוצרים המומלצים על ידי מדריכת הליווי ההתפתחותי</p>-->
+    <!--    <p>.מוצרים המתאימים ביותר להורים וילדיהם המומלצים בהתאם לגילאים ושלבי התפתחות שונים</p>-->
+    <!--    <img src="/media/images/baby1.jpg" class="img-responsive margin" style="width:100%" alt="Image">-->
+    <!--</div>-->
+
+     <div class="col-sm-4">
+        <h3 class="margin">קטלוג מוצרים</h3>
+        <p>קטלוג המוצרים המומלצים על ידי מדריכת הליווי ההתפתחותי</p>
+        <p>.מוצרים המתאימים ביותר להורים וילדיהם המומלצים בהתאם לגילאים ושלבי התפתחות שונים</p>
+        <!--<a href="/includes/php/Products.php"><img src="/media/images/baby1.jpg" class="img-responsive margin" style="width:100%" alt="Image"></a>-->
+    </div>
+
+
+  </div>
+</div>
+</body>
+</html>
